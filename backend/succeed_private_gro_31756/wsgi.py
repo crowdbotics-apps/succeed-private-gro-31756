c79daeeb815ee3db8a4fdc8c70ddd2d3bde4b4ff
Crowdbotics <team@crowdbotics.com>
2021-10-30 04:42:35 +0000

    Initial commit.

---
+++ backend/succeed_private_gro_31756/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for succeed_private_gro_31756 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'succeed_private_gro_31756.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "succeed_private_gro_31756",
+  url: "https://succeed_private_gro_31756.botics.co",
+  api: "https://succeed_private_gro_31756.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
